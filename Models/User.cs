496d589de305ce294673fe2f8ba0100ec1f9242f
ac148776 <ac148776@8CG8163L09>
2025-06-12 11:10:20 +1200

    created the user class

--- Models/User.cs
+++ Models/User.cs
@@ -8,5 +8,31 @@ namespace RecipeApp.Models
 {
     internal class User
     {
+        /*CREATE TABLE tblUser (
+        UserID int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+        FirstName VARCHAR(30),
+LastName VARCHAR(50),
+Email VARCHAR(255),
+UserName VARCHAR(255),
+RoleID int NOT NULL FOREIGN KEY REFERENCES tblRole(RoleID),
+Password VARCHAR(255));
+GO*/
+        public int userID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public int roleID { get; set; }
+        public string Password { get; set; }
+        
+        public User(int uID, string Fname, string Lname, string Uname, int rID, string Pword)
+        {
+            userID = uID;
+            FirstName = Fname;
+            LastName = Lname;
+            UserName = Uname;
+            roleID = rID;
+            Password = Pword;
+        }
+
     }
 }
