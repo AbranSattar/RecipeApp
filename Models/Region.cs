724fa2a327a21de748ee15ff62821d9f24eb135d
ac148776 <ac148776@8CG8163L09>
2025-06-12 10:34:25 +1200

    coded region class.

--- Models/Region.cs
+++ Models/Region.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace RecipeApp.Models
 {
-    internal class Region
+    public class Region
     {
+        public int regionID { get; set; }
+        public int countryID { get; set; }
+        public string Area { get; set; }
+
+        public Region(int rgID, int cID, string a)
+        {
+            regionID = rgID;
+            countryID = cID;
+            Area = a;
+        }
     }
 }
