7a69d1518358715ba9989ece657d85d069892f60
ac148776 <ac148776@8CG8163L09>
2025-06-12 10:41:56 +1200

    coded suburb class

--- Models/Suburb.cs
+++ Models/Suburb.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace RecipeApp.Models
 {
-    internal class Suburb
+    public class Suburb
     {
+        public int suburbID { get; set; }
+        public int cityID { get; set; }
+        public string suburb { get; set; }
+        public int Zipcode { get; set; }
+
+        public Suburb(int sID, int cID, string su, int zip)
+        {
+            suburbID = sID;
+            cityID = cID;
+            suburb = su;
+            Zipcode = zip;
+        }
+
     }
 }
